c3774e0002f231cffa4b93ac9ce3c18105fae58f
Ahmad Awad <ahmad.awad.business@gmail.com>
2024-05-26 14:35:27 +0300

    Implement Central Logging Service

--- Auth/Services/MembershipService.cs
+++ Auth/Services/MembershipService.cs
@@ -3,29 +3,35 @@ using Auth.Common.Constants;
 using Auth.Infrastructure;
 using Auth.Models.Membership;
 using Auth.Repositories;
+using Auth.Models.User;
 
 namespace Auth.Services
 {
     public class MembershipService : IServices
     {
         private readonly IRepository _repository;
+        private string errorMessage = string.Empty;
+        private bool isSuccess = true;
         public MembershipService(IRepository repository) 
         {
             _repository = repository;
         }
         public MembershipResult SignIn(SigninDTO signinDTO)
         {
-            string errorMessage = string.Empty;
-            bool isSuccess = true;
             try
             {
                 string hashedPassword = Tools.HashPassword(signinDTO.Password);
-                var user = _repository.Users.FirstOrDefault(user => user.Phone == signinDTO.Phone && user.Password == hashedPassword);
+                var user = _repository.GetUserByPhone(signinDTO.Phone);
 
                 if (user == null)
                 {
                     isSuccess = false;
                     errorMessage = GetErrorMessage(ConstantsEnums.ErrorMessages.UserNotFound);
+                }
+                else if (user.Result.Password != hashedPassword) 
+                {
+                    isSuccess = false;
+                    errorMessage = GetErrorMessage(ConstantsEnums.ErrorMessages.WrongPassword);
                 }
 
                 return new MembershipResult { IsSuccess = isSuccess, ErrorMessage = errorMessage };
@@ -38,7 +44,34 @@ namespace Auth.Services
 
         public MembershipResult SignUp(SignupDTO signupDTO)
         {
-            throw new NotImplementedException();
+            try
+            {
+                string hashedPassword = Tools.HashPassword(signupDTO.Password);
+                string userId = Guid.NewGuid().ToString();
+                var user = _repository.GetUserByPhone(signupDTO.Phone);
+
+
+                if (user != null)
+                {
+                    isSuccess = false;
+                    errorMessage = GetErrorMessage(ConstantsEnums.ErrorMessages.PhoneIsUsed);
+                    return new MembershipResult { IsSuccess = isSuccess, ErrorMessage = errorMessage };
+                }
+
+                _repository.AddNewUser(new UserDTO { 
+                    Id = userId,
+                    Password = hashedPassword, 
+                    FirstName = signupDTO.FirstName, 
+                    LastName = signupDTO.LastName,
+                    Phone = signupDTO.Phone
+                });
+
+                return new MembershipResult { IsSuccess = isSuccess, ErrorMessage = errorMessage };
+            }
+            catch (Exception ex)
+            {
+                return new MembershipResult { IsSuccess = false, ErrorMessage = GetErrorMessage(ConstantsEnums.ErrorMessages.InternalServerError) };
+            }
         }
         
         private string GetErrorMessage(ConstantsEnums.ErrorMessages messageKey)
